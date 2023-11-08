e8fba3a9ce0f3c2337bdbc6979fe429f497b7dbd
christianosilvaf <christianosilvaf@gmail.com>
2023-11-07 22:43:15 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const movieRouter=require('./movie.router');
+const genreRouter=require('./genre.router');
+const actorRouter=require('./actor.router');
+const directorRouter=require('./director.router');
 
 // colocar las rutas aquí
+router.use("/movies", movieRouter);
 
+router.use("/genres", genreRouter);
+
+router.use("/actors", actorRouter);
+
+router.use("/directors", directorRouter);
 
 module.exports = router;
---
+++ src/routes/movie.router.js
@@ -0,0 +1,27 @@
+const { getAll, create, getOne, remove, 
+        update,setMovieDirectors,
+        setMovieActors,
+        setMovieGenres } = require('../controllers/movie.controllers');
+const express = require('express');
+
+const movieRouter = express.Router();
+
+movieRouter.route('/')
+    .get(getAll)
+    .post(create);
+
+movieRouter.route('/:id')
+    .get(getOne)
+    .delete(remove)
+    .put(update);
+
+movieRouter.route('/:id/directors')
+    .post(setMovieDirectors);
+
+movieRouter.route('/:id/actors')
+    .post(setMovieActors);
+
+movieRouter.route('/:id/genres')
+    .post(setMovieGenres);
+
+module.exports = movieRouter;
